8a76397f13cea7c1623aac16fe824fbe60e44920
Daniel Zikry <d032410274@student.utem.edu.my>
2026-06-11 23:02:20 +0800

    improve page style

--- requestApproval.js
+++ requestApproval.js
@@ -1,20 +1,32 @@
-function approveRequest(index) {
-    let confirmApprove = confirm("Are you sure you want to approve this request?");
+function approveRequest(index)
+{
+    let confirmApprove =
+        confirm("Are you sure you want to approve this request?");
 
-    if (confirmApprove) {
+    if(confirmApprove)
+    {
         document.getElementById("actionStatus" + index).innerHTML =
             "<span class='approved-text'>Approved</span>";
+
+        document.getElementById("actionButton" + index).innerHTML =
+            "-";
 
         alert("Request approved successfully!");
     }
 }
 
-function rejectRequest(index) {
-    let confirmReject = confirm("Are you sure you want to reject this request?");
+function rejectRequest(index)
+{
+    let confirmReject =
+        confirm("Are you sure you want to reject this request?");
 
-    if (confirmReject) {
+    if(confirmReject)
+    {
         document.getElementById("actionStatus" + index).innerHTML =
             "<span class='rejected-text'>Rejected</span>";
+
+        document.getElementById("actionButton" + index).innerHTML =
+            "-";
 
         alert("Request rejected successfully!");
     }
