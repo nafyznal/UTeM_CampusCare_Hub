8e7e45415b887948c5993ee72fc57c581b271361
Daniel Zikry <d032410274@student.utem.edu.my>
2026-06-11 13:40:50 +0800

    imrpove status handling

--- requestApproval.js
+++ requestApproval.js
@@ -1,9 +1,17 @@
 function approveRequest(index) {
-    document.getElementById("actionStatus" + index).innerHTML =
-        "<span class='approved-text'>Approved</span>";
+    let confirmApprove = confirm("Are you sure you want to approve this request?");
+
+    if (confirmApprove) {
+        document.getElementById("actionStatus" + index).innerHTML =
+            "<span class='approved-text'>Approved</span>";
+    }
 }
 
 function rejectRequest(index) {
-    document.getElementById("actionStatus" + index).innerHTML =
-        "<span class='rejected-text'>Rejected</span>";
+    let confirmReject = confirm("Are you sure you want to reject this request?");
+
+    if (confirmReject) {
+        document.getElementById("actionStatus" + index).innerHTML =
+            "<span class='rejected-text'>Rejected</span>";
+    }
 }
