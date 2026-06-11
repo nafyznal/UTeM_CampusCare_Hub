daef31472c9874cdae83193070162cbe89b42ddf
Daniel Zikry <d032410274@student.utem.edu.my>
2026-06-11 20:34:06 +0800

    Added header footer and format css

--- requestApproval.php
+++ requestApproval.php
@@ -1,98 +1,32 @@
-<?php
-$requests = [
-    [
-        "time" => "19/03/2026 11:45 AM",
-        "type" => "Mini food kit",
-        "requestor" => "Lutfi Hadi"
-    ],
-    [
-        "time" => "20/03/2026 09:30 AM",
-        "type" => "Basic necessities",
-        "requestor" => "Aiman Hakim"
-    ],
-    [
-        "time" => "21/03/2026 02:15 PM",
-        "type" => "Mini food kit",
-        "requestor" => "Nur Alya"
-    ]
-];
-?>
+<?php include 'header.php'; ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Request Approval</title>
-    <link rel="stylesheet" href="requestApproval.css">
-    <script src="requestApproval.js"></script>
-</head>
-<body>
+<main class="approval-main">
+    <div class="approval-box">
+        <h2>Request Approval</h2>
 
-<div class="page">
+        <table>
+            <tr>
+                <th>Time</th>
+                <th>Type</th>
+                <th>Requestor</th>
+                <th>Status</th>
+                <th>Action</th>
+            </tr>
 
-    <div class="top-title">APPROVAL</div>
-
-    <div class="menu-icon">☰</div>
-    <div class="home-icon">⌂</div>
-
-    <div class="sidebar">
-
-        <div class="profile">
-            <div class="profile-icon">◎</div>
-            <div>
-                Hi!<br>
-                Admin
-            </div>
-        </div>
-
-        <div class="menu">
-            <a href="#">Dashboard</a>
-            <a href="requestApproval.php">Approval</a>
-            <a href="#">Item</a>
-        </div>
-
-        <div class="logout">
-            <a href="#">Logout</a>
-        </div>
-
+            <tr>
+                <td>19/03/2026 11:45 AM</td>
+                <td>Mini Food Kit</td>
+                <td>Lutfi Hadi</td>
+                <td id="actionStatus1">Pending</td>
+                <td>
+                    <button onclick="approveRequest(1)">Approve</button>
+                    <button onclick="rejectRequest(1)">Reject</button>
+                </td>
+            </tr>
+        </table>
     </div>
+</main>
 
-    <div class="content">
+<script src="requestApproval.js"></script>
 
-        <div class="approval-box">
-
-            <h1>Approval</h1>
-
-            <div class="table-section">
-
-                <table>
-                    <tr>
-                        <th>Time</th>
-                        <th>Type</th>
-                        <th>Requestor</th>
-                        <th>Actions</th>
-                    </tr>
-
-                    <?php foreach ($requests as $index => $request) { ?>
-                    <tr>
-                        <td><?php echo $request["time"]; ?></td>
-                        <td><?php echo $request["type"]; ?></td>
-                        <td><?php echo $request["requestor"]; ?></td>
-                        <td id="actionStatus<?php echo $index; ?>">
-                            <button class="approve-btn" onclick="approveRequest(<?php echo $index; ?>)">Approve</button>
-                            <button class="reject-btn" onclick="rejectRequest(<?php echo $index; ?>)">Reject</button>
-                        </td>
-                    </tr>
-                    <?php } ?>
-
-                </table>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</div>
-
-</body>
-</html>
+<?php include 'footer.php'; ?>
