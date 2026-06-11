490b82a515eb7004e989ddfe8aa0aeaab41de6ba
Daniel Zikry <d032410274@student.utem.edu.my>
2026-06-11 22:01:14 +0800

    Added approval table styling and status handling

--- requestApproval.js
+++ requestApproval.js
@@ -4,6 +4,8 @@ function approveRequest(index) {
     if (confirmApprove) {
         document.getElementById("actionStatus" + index).innerHTML =
             "<span class='approved-text'>Approved</span>";
+
+        alert("Request approved successfully!");
     }
 }
 
@@ -13,5 +15,7 @@ function rejectRequest(index) {
     if (confirmReject) {
         document.getElementById("actionStatus" + index).innerHTML =
             "<span class='rejected-text'>Rejected</span>";
+
+        alert("Request rejected successfully!");
     }
 }
