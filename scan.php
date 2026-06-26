c061e2d9bb4e6b6c44cb4b992c14c47ada89369a
nafisahznal <nafyznal@gmail.com>
2026-06-26 13:17:17 +0800

    update headerScan.php

--- scan.php
+++ scan.php
@@ -25,7 +25,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Scan and Collect</title>
 </head>
-<link rel="stylesheet" href="format.css">
+<link rel="stylesheet" href="formatScan.css">
 <body>
     <?php 
     include'headerScan.php';
